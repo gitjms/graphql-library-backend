6759ae28cda3e264c6a2fa420956fd91d121fbe1
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-24 16:11:50 +0300

    fix

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const server = new ApolloServer({
   }
 })
 
-server.listen(config.PORT || 4000).then(({ url, subscriptionsUrl }) => {
+server.listen(config.PORT).then(({ url, subscriptionsUrl }) => {
   console.log(`Server ready at ${url}`)
   console.log(`Subscriptions ready at ${subscriptionsUrl}`)
 })
