8f1b6984098d4dbe662600b09c591768b6983673
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-24 17:53:13 +0300

    fix

---
+++ build/precache-manifest.ca87ba370fa994863ec96e7e35e98645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c214c4f64411b1adb071d196b25eb965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c77d7adb4eafabc666e",
+    "url": "/static/css/main.7bc10594.chunk.css"
+  },
+  {
+    "revision": "83ddd81bce8a351c429d",
+    "url": "/static/js/2.55313534.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.55313534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c77d7adb4eafabc666e",
+    "url": "/static/js/main.ccff3f95.chunk.js"
+  },
+  {
+    "revision": "3efdb9b3d97206bcbb68",
+    "url": "/static/js/runtime-main.c4bc0995.js"
+  }
+]);
