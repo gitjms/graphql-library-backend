40a1e5fb569164f08a61f89f2c4db2ce15edd9d4
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-24 17:32:26 +0300

    fix

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.use(express.static('public'))
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://graphql-library-jms.herokuapp.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+})
 
 const JWT_SECRET = config.SECRET
 const MONGODB_URI = config.MONGODB_URI
