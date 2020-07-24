6dfddf5af7cb5842f11ff3307f77244034c9ee65
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-24 16:41:49 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { ApolloServer } = require('apollo-server')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const path = require('path')
 const typeDefs = require('./schema/typedefs')
 const resolvers = require('./schema/resolvers')
 
@@ -7,8 +11,14 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const User = require('./models/user')
+
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
+
+app.use(cors())
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
 
 const JWT_SECRET = config.SECRET
 const MONGODB_URI = config.MONGODB_URI
@@ -45,7 +55,12 @@ const server = new ApolloServer({
   }
 })
 
-server.listen(config.PORT || 4000).then(({ url, subscriptionsUrl }) => {
+server.applyMiddleware({
+  path: '/', // you should change this to whatever you want
+  app,
+})
+
+app.listen(config.PORT || 4000).then(({ url, subscriptionsUrl }) => {
   console.log(`Server ready at ${url}`)
   console.log(`Subscriptions ready at ${subscriptionsUrl}`)
 })
