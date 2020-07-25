d692c90feb230da930d81e2e18dcaa5b9b104f40
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-25 14:33:57 +0300

    heroku

--- index.js
+++ index.js
@@ -3,29 +3,38 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const http = require('http')
+const jwt = require('jsonwebtoken')
+
 const typeDefs = require('./schema/typedefs')
 const resolvers = require('./schema/resolvers')
-
-const jwt = require('jsonwebtoken')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 const User = require('./models/user')
 
+const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
 const JWT_SECRET = config.SECRET
 const MONGODB_URI = config.MONGODB_URI
+const PORT = config.PORT || 4000
+
+logger.info('connecting to', MONGODB_URI)
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const app = express()
-app.use(cors())
-app.use(express.static('build'))
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 })
 app.use((req, res, next) => {
+  cors()
+  express.static('build')
   res.header(
     "Access-Control-Allow-Origin",
     "http://graphql-library-jms.herokuapp.com"
@@ -37,23 +46,13 @@ app.use((req, res, next) => {
   next()
 })
 
-logger.info('connecting to', MONGODB_URI)
-
-mongoose.connect( MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  // engine: {    
-  //   reportSchema: true
-  // },
+  engine: {    
+    reportSchema: true
+  },
   playground: process.env.NODE_ENV !== 'production',
   context: async ({ req }) => {
     const auth = req ? req.headers.authorization : null
@@ -69,13 +68,12 @@ const server = new ApolloServer({
     }
   }
 })
-// server.applyMiddleware({ app })
+
 server.applyMiddleware({
-  path: '/', // you should change this to whatever you want
+  path: '/',
   app,
 })
 
-const PORT = Number(config.PORT || 4000)
 const httpServer = http.createServer(app);
 server.installSubscriptionHandlers(httpServer)
 
@@ -84,12 +82,3 @@ httpServer.listen(PORT, () => {
   logger.info(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
   logger.info(`Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`)
 })
-
-// server.listen({ port: config.PORT || 4000 }).then(({ url, subscriptionsUrl }) => {
-//   console.log(`Server ready at ${url}`)
-//   console.log(`Subscriptions ready at ${subscriptionsUrl}`)
-// })
-
-// app.listen(3000, () => {
-//   console.log('Go to http://localhost:3000/graphiql to run queries!');
-// })
