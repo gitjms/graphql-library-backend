3418a689996643d69d564bd73a242b9e3b3d5caf
gitjms <46410240+gitjms@users.noreply.github.com>
2020-07-25 15:39:48 +0300

    heroku

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "ws://localhost:4000"
+    "http://graphql-library-jms.herokuapp.com/graphql"
   );
   res.header(
     "Access-Control-Allow-Headers",
